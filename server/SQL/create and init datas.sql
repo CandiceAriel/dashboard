0817cf864fdfcd8dbd6307a09a3534a146ae7a85
Candice Ariel <candicewangsa@gmail.com>
2023-09-10 22:22:52 +0700

    responsive

--- server/SQL/create and init datas.sql
+++ server/SQL/create and init datas.sql
@@ -13,8 +13,8 @@ CREATE TABLE tasks(
     CONSTRAINT FK_Status FOREIGN KEY (task_status) REFERENCES status(id)
 );
 
-INSERT INTO status(status)
-VALUES ('To Do');
+INSERT INTO status(text)
+VALUES ('toDo');
 
 INSERT INTO status(status)
 VALUES ('In Progress');
@@ -25,4 +25,20 @@ VALUES ('On Review');
 INSERT INTO status(status)
 VALUES ('Done');
 
-SELECT * FROM status;
+SELECT * FROM tasks;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+flush privileges;
+
+INSERT INTO tasks(task_status, title, description)
+VALUES (1, 'Meeting with Developer', 'Report on new updates');
+
+INSERT INTO `to_do`.`tasks`
+(`task_status`,
+`title`,
+`description`)
+VALUES
+(2,
+"Feature Update",
+"Add new feature");
+
